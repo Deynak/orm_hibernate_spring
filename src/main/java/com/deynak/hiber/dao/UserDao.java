f17071e86e9bee6ba9e1dfeebf74fb22612af0a8
Deynak <miron-ox@mail.ru>
2024-08-06 00:09:37 +0300

    second commit

--- src/main/java/com/deynak/hiber/dao/UserDao.java
+++ src/main/java/com/deynak/hiber/dao/UserDao.java
@@ -5,6 +5,7 @@ import com.deynak.hiber.model.User;
 import java.util.List;
 
 public interface UserDao {
-   void add(User user);
-   List<User> listUsers();
+    void add(User user);
+
+    List<User> listUsers();
 }
